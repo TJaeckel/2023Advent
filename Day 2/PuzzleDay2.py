9b2a225a06cf6e7fdcabe69831458689832ef903
Tony Jaeckel <tonyjaeckel@gmail.com>
2023-12-03 14:18:00 -0600

    Finished Part 2

--- Day 2/PuzzleDay2.py
+++ Day 2/PuzzleDay2.py
@@ -9,26 +9,25 @@ import re
 
 path = r"C:\Users\tonyj\OneDrive\Documents\Python Scripts\Advent 2023\Day 2\day2.txt"
 
-max_colors = {"red":12, "green":13, "blue":14}
+
 
 games = open(path).readlines()
 total = 0
 for game in games:
+    max_colors = {"red":0, "green":0, "blue":0}
     gamePass = True
     gamePlayed = game.split(":")
-
     roundList = gamePlayed[1].split(";")
+    
     for gameRound in roundList:
-        if not gamePass:
-            break
+
         hands = gameRound.split(",")
         for hand in hands:
-            if not gamePass:
-                break
+
             pull = hand.strip().split(" ")
             if int(pull[0].strip()) > max_colors[pull[1].strip()]:
-                gamePass = False
-    if gamePass:
-        total += int(re.findall("\d+", gamePlayed[0])[0])
+                max_colors[pull[1].strip()] = int(pull[0].strip())
+    values = list(max_colors.values())
+    total += (values[0] * values[1] * values[2])
 print(total)
         
